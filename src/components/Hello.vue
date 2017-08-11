2592ba244bd95d60678fddbd2c8a824404ae0b17
Benson <benson@Benson.local>
2017-08-11 10:53:33 +0800

    feat: 使用yarn管理模块

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,131 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="aaa">
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+            <Form-item label="姓名" prop="name">
+                <Input v-model="formValidate.name" :show-message="false" placeholder="请输入姓名"></Input>
+            </Form-item>
+            <Form-item label="邮箱" prop="mail">
+                <Input v-model="formValidate.mail" placeholder="请输入邮箱"></Input>
+            </Form-item>
+            <Form-item label="城市" prop="city">
+                <Select v-model="formValidate.city" placeholder="请选择所在地">
+                    <Option value="beijing">北京市</Option>
+                    <Option value="shanghai">上海市</Option>
+                    <Option value="shenzhen">深圳市</Option>
+                </Select>
+            </Form-item>
+            <Form-item label="选择日期">
+                <Row>
+                    <Col span="11">
+                        <Form-item prop="date">
+                            <Date-picker type="date" placeholder="选择日期" v-model="formValidate.date"></Date-picker>
+                        </Form-item>
+                    </Col>
+                    <Col span="2" style="text-align: center">-</Col>
+                    <Col span="11">
+                        <Form-item prop="time">
+                            <Time-picker type="time" placeholder="选择时间" v-model="formValidate.time"></Time-picker>
+                        </Form-item>
+                    </Col>
+                </Row>
+            </Form-item>
+            <Form-item label="性别" prop="gender">
+                <Radio-group v-model="formValidate.gender">
+                    <Radio label="male">男</Radio>
+                    <Radio label="female">女</Radio>
+                </Radio-group>
+            </Form-item>
+            <Form-item label="爱好" prop="interest">
+                <Checkbox-group v-model="formValidate.interest">
+                    <Checkbox label="吃饭"></Checkbox>
+                    <Checkbox label="睡觉"></Checkbox>
+                    <Checkbox label="跑步"></Checkbox>
+                    <Checkbox label="看电影"></Checkbox>
+                </Checkbox-group>
+            </Form-item>
+            <Form-item label="介绍" prop="desc">
+                <Input v-model="formValidate.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="请输入..."></Input>
+            </Form-item>
+            <Form-item>
+                <Button type="primary" @click="handleSubmit('formValidate')">提交</Button>
+                <Button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
+            </Form-item>
+        </Form>
+        <div class="bbb">
+            <span class="cc">adsf</span>
+        </div>
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'hello',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
-};
+    export default {
+        data () {
+            return {
+                formValidate: {
+                    name: '',
+                    mail: '',
+                    city: '',
+                    gender: '',
+                    interest: [],
+                    date: '',
+                    time: '',
+                    desc: ''
+                },
+                ruleValidate: {
+                    name: [
+                        { required: true, message: '姓名不能为空', trigger: 'blur', error: 'adssd' }
+                    ],
+                    mail: [
+                        { required: true, message: '邮箱不能为空', trigger: 'blur' },
+                        { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+                    ],
+                    city: [
+                        { required: true, message: '请选择城市', trigger: 'change' }
+                    ],
+                    gender: [
+                        { required: true, message: '请选择性别', trigger: 'change' }
+                    ],
+                    interest: [
+                        { required: true, type: 'array', min: 1, message: '至少选择一个爱好', trigger: 'change' },
+                        { type: 'array', max: 2, message: '最多选择两个爱好', trigger: 'change' }
+                    ],
+                    date: [
+                        { required: true, type: 'date', message: '请选择日期', trigger: 'change' }
+                    ],
+                    time: [
+                        { required: true, type: 'date', message: '请选择时间', trigger: 'change' }
+                    ],
+                    desc: [
+                        { required: true, message: '请输入个人介绍', trigger: 'blur' },
+                        { type: 'string', min: 20, message: '介绍不能少于20字', trigger: 'blur' }
+                    ]
+                }
+            }
+        },
+        methods: {
+            handleSubmit (name) {
+                this.$refs.formValidate.validateField('name', (e) => {
+                    console.log(e)
+                })
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                        this.$Message.success('提交成功!')
+                    } else {
+                        this.$Message.error('表单验证失败!')
+                    }
+                })
+            },
+            handleReset (name) {
+                this.$Message.warning('asdfasd')
+                this.$refs[name].resetFields()
+            }
+        }
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped lang="less">
+    .bbb{
+        .cc{
+          font-size: 50px;  
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+import 'babel-polyfill'
+import Vue from 'vue'
+import fastclick from 'fastclick'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'  // 使用css
+import App from './App'
+import router from './router'
 
-Vue.config.productionTip = false;
+fastclick.attach(document.body)
+
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App),
-});
+    el: '#app',
+    router,
+    render: h => h(App)
+})
